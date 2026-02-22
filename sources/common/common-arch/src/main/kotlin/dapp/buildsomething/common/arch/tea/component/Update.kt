b7a7fe47dffe0633f72a4ef54f39edd3f88e1f6f
zeroxpunk <punk.dvpn@proton.me>
2026-02-23 00:42:34 +0200

    Add TEA components: Actor, Reducer, Renderer, UiStateMapper

---
+++ sources/common/common-arch/src/main/kotlin/dapp/buildsomething/common/arch/tea/component/Update.kt
@@ -0,0 +1,7 @@
+package dapp.buildsomething.common.arch.tea.component
+
+class Update<Command : Any, Effect : Any, State : Any>(
+    val state: State? = null,
+    val commands: List<Command> = emptyList(),
+    val effects: List<Effect> = emptyList(),
+)
