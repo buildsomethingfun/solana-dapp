f52733a93ab323b9b3f5f210f9eba17a8ffbe3b5
zeroxpunk <punk.dvpn@proton.me>
2026-03-02 09:41:00 +0300

    Update backend URL to beta environment

--- sources/common/common-network/src/main/kotlin/dapp/buildsomething/common/network/backendurl/BackendUrl.kt
+++ sources/common/common-network/src/main/kotlin/dapp/buildsomething/common/network/backendurl/BackendUrl.kt
@@ -7,11 +7,13 @@ sealed interface BackendUrl {
     val app: String
 
     data object Dev : BackendUrl {
-        override val app = "https://backend-kappa-nine-81.vercel.app/"
+//        override val app = "https://backend-kappa-nine-81.vercel.app/"
+//        override val app = "https://beta.buildsomething.fun/"
+        override val app = "https://backend-phi-seven-52.vercel.app/"
     }
 
     data object Prod : BackendUrl {
-        override val app = "https://backend-kappa-nine-81.vercel.app/"
+        override val app = "https://app.buildsomething.fun/"
     }
 
     @Suppress("KotlinConstantConditions")
