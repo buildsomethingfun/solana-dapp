4088a41af7e3bbdc00205c410b534be43eeba0c3
zeroxpunk <punk.dvpn@proton.me>
2026-03-02 11:30:00 +0300

    Store refresh token in preferences alongside access token

--- sources/repository/repository-something/src/main/kotlin/dapp/buildsomething/repository/something/internal/jwt/TokenPreference.kt
+++ sources/repository/repository-something/src/main/kotlin/dapp/buildsomething/repository/something/internal/jwt/TokenPreference.kt
@@ -3,9 +3,11 @@ package dapp.buildsomething.repository.something.internal.jwt
 import androidx.annotation.Keep
 import dapp.buildsomething.repository.preferences.Preference
 import dapp.buildsomething.repository.something.internal.jwt.model.JwtToken
+import dapp.buildsomething.repository.user.model.User
 
 @Keep
 internal data object AccessTokenPreference : Preference<JwtToken> {
     override val key: String = "Key:AccessToken"
     override val defaultValue: JwtToken? = null
+    override val serializer = JwtToken.serializer()
 }
