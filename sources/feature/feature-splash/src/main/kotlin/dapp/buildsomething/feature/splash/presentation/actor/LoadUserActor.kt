c96896f3fdf02d524786256a334236daca886f0e
zeroxpunk <punk.dvpn@proton.me>
2026-03-04 10:50:00 +0300

    Check auth and onboarding state in splash actor

--- sources/feature/feature-splash/src/main/kotlin/dapp/buildsomething/feature/splash/presentation/actor/LoadUserActor.kt
+++ sources/feature/feature-splash/src/main/kotlin/dapp/buildsomething/feature/splash/presentation/actor/LoadUserActor.kt
@@ -2,6 +2,7 @@ package dapp.buildsomething.feature.splash.presentation.actor
 
 import dapp.buildsomething.common.arch.tea.component.Actor
 import dapp.buildsomething.feature.splash.presentation.model.SplashEvent
+import dapp.buildsomething.repository.something.internal.jwt.JwtRepository
 import dapp.buildsomething.repository.user.UserRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterIsInstance
@@ -12,11 +13,18 @@ import dapp.buildsomething.feature.splash.presentation.model.SplashEvent as Even
 
 internal class LoadUserActor(
     private val userRepository: UserRepository,
+    private val tokenProvider: JwtRepository,
 ) : Actor<Command, Event> {
 
     override fun act(commands: Flow<Command>): Flow<Event> {
         return commands.filterIsInstance<Command.LoadOnboardingState>()
-            .flatMapLatest { userRepository.user.mapLatest { user -> !user.anonymous } }
+            .flatMapLatest {
+                userRepository.user
+                    .mapLatest { user ->
+                        val jwt = tokenProvider.getToken()
+                        !user.anonymous && jwt != null
+                    }
+            }
             .mapLatest(SplashEvent::AuthStateLoaded)
     }
 }
