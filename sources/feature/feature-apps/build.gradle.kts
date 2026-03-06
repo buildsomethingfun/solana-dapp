8d333ed34de458c6c73ee84fadd8b20ebb3be3ec
zeroxpunk <punk.dvpn@proton.me>
2026-03-06 15:55:00 +0300

    Wire app detail actors and store into apps DI module

--- sources/feature/feature-apps/build.gradle.kts
+++ sources/feature/feature-apps/build.gradle.kts
@@ -6,10 +6,13 @@ plugins {
 android.namespace = "dapp.buildsomething.feature.apps"
 
 dependencies {
-    implementation(projects.sources.common.commonUi)
     implementation(projects.sources.common.commonArch)
     implementation(projects.sources.common.commonNavigation)
+    implementation(projects.sources.common.commonUi)
+    implementation(projects.sources.common.commonUtil)
+    implementation(projects.sources.repository.repositorySomething)
     implementation(libs.androidxAppcompat)
     implementation(libs.coilCompose)
+    implementation(libs.timber)
     implementation(libs.koinAndroid)
 }
--- sources/feature/feature-apps/src/main/kotlin/dapp/buildsomething/feature/apps/di/AppsModule.kt
+++ sources/feature/feature-apps/src/main/kotlin/dapp/buildsomething/feature/apps/di/AppsModule.kt
@@ -1,12 +1,19 @@
 package dapp.buildsomething.feature.apps.di
 
-import dapp.buildsomething.feature.apps.presentation.AppsReducer
-import dapp.buildsomething.feature.apps.presentation.AppsStore
-import dapp.buildsomething.feature.apps.presentation.AppsStoreProvider
-import dapp.buildsomething.feature.apps.presentation.AppsUiStateMapper
-import dapp.buildsomething.feature.apps.presentation.actor.LoadAppsActor
-import dapp.buildsomething.feature.apps.presentation.model.AppsState
-import dapp.buildsomething.feature.apps.presentation.model.AppsUIEvent
+import dapp.buildsomething.feature.apps.details.presentation.AppDetailReducer
+import dapp.buildsomething.feature.apps.details.presentation.AppDetailStore
+import dapp.buildsomething.feature.apps.details.presentation.AppDetailStoreProvider
+import dapp.buildsomething.feature.apps.details.presentation.AppDetailUiStateMapper
+import dapp.buildsomething.feature.apps.details.presentation.actor.DeployAppActor
+import dapp.buildsomething.feature.apps.details.presentation.actor.LoadAppDetailActor
+import dapp.buildsomething.feature.apps.details.presentation.actor.PublishFlowActor
+import dapp.buildsomething.feature.apps.details.presentation.model.AppDetailState
+import dapp.buildsomething.feature.apps.list.presentation.AppsReducer
+import dapp.buildsomething.feature.apps.list.presentation.AppsStore
+import dapp.buildsomething.feature.apps.list.presentation.AppsStoreProvider
+import dapp.buildsomething.feature.apps.list.presentation.AppsUiStateMapper
+import dapp.buildsomething.feature.apps.list.presentation.actor.LoadAppsActor
+import dapp.buildsomething.feature.apps.list.presentation.model.AppsState
 import org.koin.dsl.module
 
 val AppsModule = module {
@@ -15,10 +22,25 @@ val AppsModule = module {
             override fun provide(): AppsStore {
                 return AppsStore(
                     initialState = AppsState(),
-                    actors = setOf(LoadAppsActor()),
+                    actors = setOf(LoadAppsActor(appsInteractor = get())),
                     reducer = AppsReducer(),
-                    initialEvents = listOf(AppsUIEvent.LoadApps),
                     uiStateMapper = AppsUiStateMapper(),
+                )
+            }
+        }
+    }
+    factory<AppDetailStoreProvider> {
+        object : AppDetailStoreProvider() {
+            override fun provide(): AppDetailStore {
+                return AppDetailStore(
+                    initialState = AppDetailState(),
+                    actors = setOf(
+                        LoadAppDetailActor(appsInteractor = get()),
+                        DeployAppActor(appsInteractor = get()),
+                        PublishFlowActor(appsInteractor = get()),
+                    ),
+                    reducer = AppDetailReducer(),
+                    uiStateMapper = AppDetailUiStateMapper(),
                 )
             }
         }
