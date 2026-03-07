b7a5c542d7fc7696dadc18f0d72f963f044f918b
zeroxpunk <punk.dvpn@proton.me>
2026-03-07 12:45:00 +0300

    Add navigation and repository dependencies to newapp module

--- sources/feature/feature-newapp/build.gradle.kts
+++ sources/feature/feature-newapp/build.gradle.kts
@@ -7,6 +7,11 @@ android.namespace = "dapp.buildsomething.feature.newapp"
 
 dependencies {
     implementation(projects.sources.common.commonUi)
+    implementation(projects.sources.common.commonArch)
     implementation(projects.sources.common.commonNavigation)
+    implementation(projects.sources.common.commonUtil)
+    implementation(projects.sources.repository.repositorySomething)
     implementation(libs.androidxAppcompat)
+    implementation(libs.koinAndroid)
+    implementation(libs.markdown)
 }
