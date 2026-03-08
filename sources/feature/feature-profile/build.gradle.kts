8094c473926a9f23234bf4e42694d2a945ca434d
zeroxpunk <punk.dvpn@proton.me>
2026-03-08 11:30:00 +0300

    Add dependencies for profile feature module

--- sources/feature/feature-profile/build.gradle.kts
+++ sources/feature/feature-profile/build.gradle.kts
@@ -7,6 +7,13 @@ android.namespace = "dapp.buildsomething.feature.profile"
 
 dependencies {
     implementation(projects.sources.common.commonUi)
+    implementation(projects.sources.common.commonArch)
     implementation(projects.sources.common.commonNavigation)
+    implementation(projects.sources.common.commonUtil)
+    implementation(projects.sources.repository.repositorySomething)
+    implementation(projects.sources.repository.repositoryOnboarding)
+    implementation(projects.sources.repository.repositorySolana)
     implementation(libs.androidxAppcompat)
+    implementation(libs.koinAndroid)
+    implementation(libs.coilCompose)
 }
